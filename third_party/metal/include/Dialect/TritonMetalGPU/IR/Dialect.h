d6a3fdfa27659fb34184b24ca4e3812ee733a641
ben594 <benli20042013@gmail.com>
2026-04-21 12:39:26 -0400

    Get new metal gpu dialect building

--- third_party/metal/include/Dialect/TritonMetalGPU/IR/Dialect.h
+++ third_party/metal/include/Dialect/TritonMetalGPU/IR/Dialect.h
@@ -1,9 +1,13 @@
 #ifndef TRITON_THIRD_PARTY_METAL_INCLUDE_DIALECT_TRITONMETALGPU_IR_DIALECT_H_
 #define TRITON_THIRD_PARTY_METAL_INCLUDE_DIALECT_TRITONMETALGPU_IR_DIALECT_H_
 
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/Dialect.h"
+#include "triton/Dialect/Triton/IR/Dialect.h"
+#include "triton/Dialect/TritonGPU/IR/Dialect.h"
+
 // clang-format off
 #include "metal/include/Dialect/TritonMetalGPU/IR/Dialect.h.inc"
-#include "metal/include/Dialect/TritonMetalGPU/IR/TritonMetalGPUEnums.h.inc"
 // clang-format on
 
 #define GET_OP_CLASSES
---
+++ third_party/metal/lib/Dialect/TritonMetalGPU/IR/Dialect.cpp
@@ -0,0 +1,19 @@
+#include "mlir/IR/BuiltinTypes.h"
+
+// clang-format off
+#include "Dialect/TritonMetalGPU/IR/Dialect.h"
+#include "Dialect/TritonMetalGPU/IR/Dialect.cpp.inc"
+// clang-format on
+
+using namespace mlir;
+using namespace mlir::triton::metalgpu;
+
+void mlir::triton::metalgpu::TritonMetalGPUDialect::initialize() {
+  addOperations<
+#define GET_OP_LIST
+#include "Dialect/TritonMetalGPU/IR/Ops.cpp.inc"
+      >();
+}
+
+#define GET_OP_CLASSES
+#include "Dialect/TritonMetalGPU/IR/Ops.cpp.inc"
