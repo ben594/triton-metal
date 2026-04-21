2c2f9b016054f2b8ccc0a3aa395e5c1ee0bce706
ben594 <benli20042013@gmail.com>
2026-04-21 11:07:38 -0400

    Start creating metal gpu dialect to make dot op conversion more similar to msl generated code

---
+++ third_party/metal/lib/Dialect/TritonMetalGPU/IR/Dialect.h
@@ -0,0 +1,12 @@
+#ifndef TRITON_THIRD_PARTY_METAL_INCLUDE_DIALECT_TRITONMETALGPU_IR_DIALECT_H_
+#define TRITON_THIRD_PARTY_METAL_INCLUDE_DIALECT_TRITONMETALGPU_IR_DIALECT_H_
+
+// clang-format off
+#include "metal/include/Dialect/TritonMetalGPU/IR/Dialect.h.inc"
+#include "metal/include/Dialect/TritonMetalGPU/IR/TritonMetalGPUEnums.h.inc"
+// clang-format on
+
+#define GET_OP_CLASSES
+#include "metal/include/Dialect/TritonMetalGPU/IR/Ops.h.inc"
+
+#endif // TRITON_THIRD_PARTY_METAL_INCLUDE_DIALECT_TRITONMETALGPU_IR_DIALECT_H_
